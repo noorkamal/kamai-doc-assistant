b6f4c292ddb774754150fc9cca65bfe29e7bce6d
noorkamal <50227821+noorkamal@users.noreply.github.com>
2025-09-15 13:42:15 -0500

    Update pdfjs.d.ts

--- src/types/pdfjs.d.ts
+++ src/types/pdfjs.d.ts
@@ -1,4 +1,4 @@
-declare module 'pdfjs-dist/legacy/build/pdf.js' {
+declare module 'pdfjs-dist/webpack' {
   export const GlobalWorkerOptions: any;
   export function getDocument(src: any, options?: any): any;
 }
